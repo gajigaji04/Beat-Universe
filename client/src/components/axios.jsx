8d40d1ff9c53dd712ec817a779a13af14b0c9ccf
gajigaji04 <gajigaji04@naver.com>
2023-11-22 23:19:21 +0900

    Axios posts

---
+++ client/src/components/axios.jsx
@@ -0,0 +1,33 @@
+// 프론트엔드의 컴포넌트 파일
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const PostList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get("/api/data");
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h2>Post List</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post._id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostList;
