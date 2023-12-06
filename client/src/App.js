a26be5998d9de0c84c9d77183865232f9c3029d0
gajigaji04 <gajigaji04@naver.com>
2023-12-06 22:47:56 +0900

    Temporary commit

--- client/src/App.js
+++ client/src/App.js
@@ -15,13 +15,13 @@ function App() {
         <div className="container">
           {/* Sidebar */}
           <Sidebar />
-          <div className="others">other pages</div>
+          {/* <div className="others">other pages</div> */}
           <main>
-            {/* <Routes>
+            <Routes className="routerPages">
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
-            </Routes> */}
+            </Routes>
           </main>
         </div>
         <Footer />
